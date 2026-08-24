5c80967e8449a4a7cea4acae53c2999b531730c9
Link4Electronics <link4electronics@gmail.com>
2026-08-24 19:53:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor
+#pacman -Syu --noconfirm
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 # Comment this out if you need an AUR package
 PRE_BUILD_CMDS="sed -i 's/ --prefix=\/usr//; s|usr/games|usr/local/share/games|' ./PKGBUILD" make-aur-package powermanga
